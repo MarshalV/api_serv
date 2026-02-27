07f24cce4b4cbffcf5f1546fd534bc225617edad
Marshal <59802952+MarshalV@users.noreply.github.com>
2026-02-27 15:21:35 +0300

    add test_.env

---
+++ test_.env/test_env.py
@@ -0,0 +1,5 @@
+# POSTGRES_USER=postgres
+# POSTGRES_PASSWORD=testadmin
+# POSTGRES_DB=main_db
+# DATABASE_URL=postgresql://postgres:testadmin@postgres:5432/main_db
+# LOCAL_DATABASE_URL=postgresql://postgres:testadmin@localhost:5433/main_db
